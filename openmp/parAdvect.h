f9094ff4e41d92ef8c38c41d3c877b124e5ae5f1
Code Help Tutor <1823890830@qq.com>
2024-05-11 02:27:23 +0000

    WeChat cstutorcs comp4300 assignment2

---
+++ openmp/parAdvect.h
@@ -0,0 +1,17 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+// OpenMP 2D advection solver module
+
+// sets up parallel parameters above
+void init_parallel_parameter_values(int M, int N, int P, int Q, int verbosity);
+
+// evolve advection over r timesteps, with (u,ldu) storing the local field
+// using a 1D decomposition
+void run_parallel_omp_advection_1D_decomposition(int r, double *u, int ldu);
+
+// 2D, wide parallel region variant
+void run_parallel_omp_advection_2D_decomposition(int r, double *u, int ldu);
+
+// extra optimization variant
+void run_parallel_omp_advection_with_extra_opts(int r, double *u, int ldu);
---
+++ openmp/test.sh
@@ -0,0 +1,5 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+export OMP_NUM_THREADS=4; ./testAdvect 10000 10000 100
+export OMP_NUM_THREADS=4; ./testAdvect -P2 10000 10000 100
